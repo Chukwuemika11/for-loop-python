b2e763995817e707138a6d5a37b15a3682aef646
Chukwuemika11 <117942512+Chukwuemika11@users.noreply.github.com>
2024-06-05 23:03:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("I love python")
+name = "python"
+print(f"I love {name}")
+choose = [1,2,3,4,5]
+for multiply in range(len(choose)):
+    choose[multiply] = choose[multiply]* 2
+print(choose)
